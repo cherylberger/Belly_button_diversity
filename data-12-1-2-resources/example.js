e96280905adde1acb37aca54400cc9bd13556a81
cherylberger <94234511+cherylberger@users.noreply.github.com>
2022-02-07 22:30:49 -0500

    Add files via upload

---
+++ data-12-1-2-resources/example.js
@@ -0,0 +1,63 @@
+var numbers = [0,2,4,6,8];
+var times5 = numbers.map(function(integer) {
+return integer * 5;
+});
+console.log(times5);
+
+var cities = [
+    {
+      "Rank": 1,
+      "City": "San Antonio ",
+      "State": "Texas",
+      "Increase_from_2016": "24208",
+      "population": "1511946"
+    },
+    {
+      "Rank": 2,
+      "City": "Phoenix ",
+      "State": "Arizona",
+      "Increase_from_2016": "24036",
+      "population": "1626078"
+    },
+    {
+      "Rank": 3,
+      "City": "Dallas",
+      "State": "Texas",
+      "Increase_from_2016": "18935",
+      "population": "1341075"
+    }
+];
+
+var cityNames = cities.map(function(city){
+    return city.City;
+});
+
+var cities = [
+    {
+      "Rank": 1,
+      "City": "San Antonio ",
+      "State": "Texas",
+      "Increase_from_2016": "24208",
+      "population": "1511946"
+    },
+    {
+      "Rank": 2,
+      "City": "Phoenix ",
+      "State": "Arizona",
+      "Increase_from_2016": "24036",
+      "population": "1626078"
+    },
+    {
+      "Rank": 3,
+      "City": "Dallas",
+      "State": "Texas",
+      "Increase_from_2016": "18935",
+      "population": "1341075"
+    }
+];
+
+
+console.log(cityPop);
+var cityPop = cities.map(function(population){
+    return count.population;
+});
---
+++ data-12-1-2-resources/plots_CB.js
@@ -0,0 +1,1 @@
+Plotly.newPlot("myPlot", [{x: [7, 14, 21], y: [5, 10, 15]}]);
